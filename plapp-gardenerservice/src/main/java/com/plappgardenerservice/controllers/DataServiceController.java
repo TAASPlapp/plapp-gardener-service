07161d1e7629a6488a597df00999df4bc4b57f1e
gegelumis <matteo.dicunzolo@edu.unito.it>
2020-02-18 23:31:34 +0100

    Aggiunte repositories e services.

--- plapp-gardenerservice/src/main/java/com/plappgardenerservice/controllers/DataServiceController.java
+++ plapp-gardenerservice/src/main/java/com/plappgardenerservice/controllers/DataServiceController.java
@@ -1,10 +1,26 @@
 package com.plappgardenerservice.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.plappgardenerservice.entities.Schedule;
+import com.plappgardenerservice.services.ScheduleService;
+import org.springframework.web.bind.annotation.*;
+import java.util.Date;
 
 @RestController
 public class DataServiceController {
 
+    /* This method is invoked whenever a user requests
+     * the insertion of a new schedule (e.g. water, prune, ...)
+     */
+    @PutMapping("/add_schedule")
+    Schedule addSchedule(@RequestParam long plantID, @RequestParam Date date, @RequestParam String action) {
+        System.out.println("INVOKED");
+        System.out.println(plantID);
+        System.out.println(action);
+        System.out.println(date);
+        ScheduleService scheduleService = new ScheduleService();
+        Schedule newSchedule = scheduleService.createSchedule(plantID, date, action);
 
+        return newSchedule;
+    }
 
 }
--- plapp-gardenerservice/src/main/java/com/plappgardenerservice/services/ScheduleNotifier.java
+++ plapp-gardenerservice/src/main/java/com/plappgardenerservice/services/ScheduleNotifier.java
@@ -3,18 +3,20 @@ package com.plappgardenerservice.services;
 import java.util.Timer;
 
 
-/* This class implements a thread whose task is to periodically notify users
+/* This class implements a thread whose task is to periodically notify users about
  * which activities they should perform onto their plants (watering, pruning, etc).
  */
 public class ScheduleNotifier implements Runnable {
 
-    long delay = 1800000; //half an hour
+    long delay = 1800; //sleep time in ms
 
     @Override
     public void run() {
         while(true){
+            //System.out.println("Schedule Notifier is online");
             //TODO check if some plant ops should be notified and notify
-            //sleep for half an hour
+
+            //take a nap
             try {
                 Thread.sleep(delay);
             } catch (InterruptedException e) {

62d2b9d9eef439c99a599c10bdf065a40415124a
gegelumis <matteo.dicunzolo@edu.unito.it>
2020-02-19 23:15:15 +0100

    Inizio implementazione thread per controllo DB e invio notifiche. Problema ad usare ScheduleService dal thread.

--- plapp-gardenerservice/src/main/java/com/plappgardenerservice/PlappGardenerServiceApplication.java
+++ plapp-gardenerservice/src/main/java/com/plappgardenerservice/PlappGardenerServiceApplication.java
@@ -1,16 +1,29 @@
 package com.plappgardenerservice;
 
-import com.plappgardenerservice.services.ScheduleNotifier;
+import com.plappgardenerservice.notifier.ScheduleNotifier;
+import com.plappgardenerservice.utils.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 @SpringBootApplication
 public class PlappGardenerServiceApplication {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PlappGardenerServiceApplication.class, args);
-		ScheduleNotifier notifier = new ScheduleNotifier();
-		notifier.run();
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		ScheduleNotifier sn = (ScheduleNotifier) ctx.getBean("scheduleNotifier");
+		sn.start();
+
+
+
 	}
 
 }
--- plapp-gardenerservice/src/main/java/com/plappgardenerservice/controllers/DataServiceController.java
+++ plapp-gardenerservice/src/main/java/com/plappgardenerservice/controllers/DataServiceController.java
@@ -2,24 +2,27 @@ package com.plappgardenerservice.controllers;
 
 import com.plappgardenerservice.entities.Schedule;
 import com.plappgardenerservice.services.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 
 @RestController
 public class DataServiceController {
 
+    @Autowired
+    ScheduleService scheduleService;
+
     /* This method is invoked whenever a user requests
      * the insertion of a new schedule (e.g. water, prune, ...)
      */
     @PutMapping("/add_schedule")
-    Schedule addSchedule(@RequestParam long plantID, @RequestParam Date date, @RequestParam String action) {
+    Schedule addSchedule(@RequestParam long plantID, @RequestParam Date date, @RequestParam String action, @RequestParam int periodicity) {
         System.out.println("INVOKED");
         System.out.println(plantID);
         System.out.println(action);
         System.out.println(date);
-        ScheduleService scheduleService = new ScheduleService();
-        Schedule newSchedule = scheduleService.createSchedule(plantID, date, action);
-
+        System.out.println(periodicity);
+        Schedule newSchedule = scheduleService.createSchedule(plantID, date, action, periodicity);
         return newSchedule;
     }
 
